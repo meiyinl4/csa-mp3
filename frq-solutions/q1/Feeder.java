1449642a99ac75a42c51d210780559bd0e8e0093
meiyinl4 <meiyinl4@nycstudents.net>
2024-05-22 09:43:13 -0400

    Update Feeder.java

--- frq-solutions/q1/Feeder.java
+++ frq-solutions/q1/Feeder.java
@@ -1,1 +1,27 @@
+public class Feeder {
+  private int currentFood;
+
+  public Feeder(int f) {
+    currentFood = f;
+  }
+
+  public void simulateOneDay(int numBirds) {
+    boolean normal = (int) (Math.random() * 100 + 1) <= 95;
+    if (normal) {
+      int gramsEaten = (int) (Math.random() * (50 - 10 + 1) + 10);
+      int totalEaten = gramsEaten * numBirds;
+      if (totalEaten > currentFood) {
+        currentFood = 0;
+      } else {
+        currentFood -= totalEaten;
+      }
+    } else {
+      currentFood = 0;
+    }
+  }
+
+  public int getCurrentFood() {
+    return currentFood;
+  }
+}
 
